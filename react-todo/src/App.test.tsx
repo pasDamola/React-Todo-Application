c405df5cfe3557cb7bce86e3e5b2c601f6103ddc
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-02-27 02:54:57 +0100

    first challenge completed

--- react-todo/src/App.test.tsx
+++ react-todo/src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Todo Application/i);
   expect(linkElement).toBeInTheDocument();
+
+  const buttonElement = screen.getByRole("button", { name: "Hello There" });
+  expect(buttonElement).toBeInTheDocument();
 });
